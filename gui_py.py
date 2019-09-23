64d66e30274414c1af034f5a4c49d4d540e1b12c
93TEI <93louis@gmail.com>
2019-09-23 16:02:35 +0900

    창띄우기 + 창배너에 아이콘 넣기

--- gui_py.py
+++ gui_py.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget # 설치 : pip3 install PyQt5
+from PyQt5.QtGui import QIcon
 
 
 class MyApp(QWidget):
@@ -11,9 +12,12 @@ class MyApp(QWidget):
 
     def initUI(self):
 
-        self.setWindowTitle('My First Application') # setWindowTitle() 메서드는 타이틀바에 나타나는 창의 제목을 설정
-        self.move(300, 300) # move() 메서드는 위젯을 스크린의 x=300px, y=300px의 위치로 이동
-        self.resize(400, 200) # 메서드는 위젯의 크기를 너비 400px, 높이 200px로 조절
+        self.setWindowTitle('정태이') # setWindowTitle() 메서드는 타이틀바에 나타나는 창의 제목을 설정
+        self.setWindowIcon(QIcon('정태이.png')) # QIcon 객체 생성 -> QIcon에 보여질 이미지를 입력// 경로입력가능
+        self.setGeometry(300,300,300,200) #앞의 두 매개변수는 창의 x, y 위치를 결정하고, 뒤의 두 매개변수는 각각 창의 너비와 높이를 결정
+
+        #self.move(300, 300) # move() 메서드는 위젯을 스크린의 x=300px, y=300px의 위치로 이동
+        #self.resize(400, 200) # 메서드는 위젯의 크기를 너비 400px, 높이 200px로 조절
         self.show() # show() 메서드는 위젯을 스크린에
 
 
